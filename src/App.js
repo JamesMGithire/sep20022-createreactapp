714ee26a75fa0a6c380bf0829f1d14dd3725e145
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-20 12:28:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>This is JSx</p>
+      </div>
+  );
+}
+
+export default App;
