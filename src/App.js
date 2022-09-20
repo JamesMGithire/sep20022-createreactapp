f7cef4104299865cd110ae6e2d03d1e0800d48a6
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-20 12:39:14 +0300

    Rendered button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
 
 function App() {
   return (
     <div>
+      <Button/>
       <h1>Welcome</h1>
       <p>This is JSx</p>
       </div>
